6456f6bc9c0a26150576804304d3fc34dcf1165e
xiaoming <xumingming340@126.com>
2016-03-06 16:00:01 +0800

    first build

---
+++ SDK-gcc/build/CMakeFiles/future_net.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/future_net.dir/route.cpp.o"
+  "CMakeFiles/future_net.dir/future_net.cpp.o"
+  "/home/xiaoming/work/huawei/FindDawn/SDK-gcc/bin/future_net.pdb"
+  "/home/xiaoming/work/huawei/FindDawn/SDK-gcc/bin/future_net"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/future_net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
